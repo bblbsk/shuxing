61f58eee4717778d790241b76cecf56fafc9d676
caishixian <caishixian@daojia-inc.com>
2018-09-11 11:54:46 +0800

    update guava

---
+++ x-continue/src/com/x/guava/common/base/StringsDemo.java
@@ -0,0 +1,13 @@
+package com.x.guava.common.base;
+
+import com.google.common.base.Strings;
+
+public class StringsDemo {
+
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty(null));
+		
+		System.out.println(Strings.emptyToNull(null));
+		
+	}
+}
---
+++ x-continue/src/com/x/guava/io/FilesDemo.java
@@ -0,0 +1,8 @@
+package com.x.guava.io;
+
+public class FilesDemo {
+
+	public static void main(String[] args) {
+		
+	}
+}
